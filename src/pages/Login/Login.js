d6d7c3015b7bff9a51ded94e72210b9df861d351
unknown <chkevi@gmail.com>
2022-05-06 15:52:51 +0900

    Add: Login UI 완성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const changeValue = e => {
+    const { name, value } = e.target;
+    setInputValues(prestate => {
+      return { ...prestate, [name]: value };
+    });
+  };
+
+  const goToMain = () => {
+    navigate('/');
+  };
+
+  const goToRegister = () => {
+    navigate('/register');
+  };
+
+  return (
+    <RegisterFormWrap>
+      <Logo onClick={goToMain}>Influence</Logo>
+      <RegisterForm>
+        <InputWrap>
+          <P>이메일</P>
+          <Input
+            type="email"
+            name="email"
+            value={inputValues.email}
+            onChange={changeValue}
+          />
+        </InputWrap>
+        <InputWrap>
+          <P>비밀번호</P>
+          <Input
+            type="password"
+            name="password"
+            value={inputValues.password}
+            onChange={changeValue}
+          />
+        </InputWrap>
+        <InputWrap>
+          <Button type="button">로그인</Button>
+          <Button type="button" onClick={goToRegister}>
+            회원가입
+          </Button>
+        </InputWrap>
+      </RegisterForm>
+    </RegisterFormWrap>
+  );
 };
 
 export default Login;
+
+const RegisterFormWrap = styled.div`
+  ${({ theme }) => theme.flex('center', 'center')};
+  flex-direction: column;
+  width: 80%;
+  height: 550px;
+  margin: 0 auto;
+`;
+
+const Logo = styled.h2`
+  font-size: 50px;
+`;
+
+const RegisterForm = styled.form`
+  ${({ theme }) => theme.flex('space-around', 'center')};
+  flex-direction: column;
+  height: 60%;
+  margin-top: 20px;
+`;
+
+const InputWrap = styled.div`
+  margin: 0 auto;
+`;
+
+const P = styled.p`
+  width: 150px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Input = styled.input`
+  width: 400px;
+  height: 40px;
+  margin-top: 10px;
+  border: 1px solid black;
+  border-radius: 3px;
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+  width: 170px;
+  height: 40px;
+  margin: 0px 20px;
+  border: none;
+  border-radius: 3px;
+  background-color: #0074e9;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+`;
