2711a3b4a05b14711ead62d4936d2b4337b8e2f0
unknown <chkevi@gmail.com>
2022-05-12 17:22:24 +0900

    Modify: 로그인 수정

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import List from './pages/List/List';
 import MyPage from './pages/MyPage/MyPage';
+import Campaign from './pages/Campaign/Campaign';
 
 const Router = () => {
   return (
@@ -17,6 +18,7 @@ const Router = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/list" element={<List />} />
         <Route path="/mypage" element={<MyPage />} />
+        {/* <Route path="/campaign" element={<Campaign />} /> */}
       </Routes>
     </BrowserRouter>
   );
